52fdc4270fc3549ee305975bbfc429602748c4eb
LouisLun <louis.jhan@area02.com>
2023-03-30 16:02:18 +0800

    [fix]修正設定檔

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'ChannelId' => env('LINE_PAY_CHANNEL_ID'),
+    'channelId' => env('LINE_PAY_CHANNEL_ID'),
     'channelSecret' => env('LINE_PAY_CHANNEL_SECRET_KEY'),
     'isSandBox' => env('LINE_PAY_IS_SANDBOX', false),
 ];
